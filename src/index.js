3940d943db77be861a0f6646cec182c3af08d1ce
Shanmugavel Sundaramoorthy <Shanmugavel@users.noreply.github.com>
2017-10-27 23:32:04 -0400

    Add code to send HTML email

---
+++ src/index.js
@@ -0,0 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+var mailgun = require('mailgun-js')({apiKey: process.env.apiKey, domain: process.env.domain});
+
+var data = {
+    from: 'Shanmugavel Sundaramoorthy <shan@shanmugavel.xyz>',
+    to: 'shanmugavel.rs@gmail.com, s.shanmugavelmca@gmail.com',
+    cc : 'ssv.mca@gmail.com',
+    subject: 'Hi!!',
+    html: `My 1st html email!!. <br />
+           Please visit my site<a href="https://www.shanmugavel.xyz">shanmugavel.xyz</a>
+           <br/><br/> 
+           Thanks,<br/>
+           Shan`
+  };
+   
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
